7f3687e4b8a53d0712fbd25bf4774f1cd0f0806e
DanSimonson <32283441+DanSimonson@users.noreply.github.com>
2019-04-18 22:40:26 +0800

    added new config file

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const Config = {
-  apiKey: "AIzaSyCRZc-DwVocH_AjseiKNTUtEs4FiXr1h-w",
-  authDomain: "products-23ff3.firebaseapp.com",
-  databaseURL: "https://products-23ff3.firebaseio.com",
-  projectId: "products-23ff3",
-  storageBucket: "products-23ff3.appspot.com",
-  messagingSenderId: "367016668249"
+    apiKey: "AIzaSyC7AnbiZol5oYL_olPmknKza_g-oKTtojI",
+    authDomain: "all-purpose-238011.firebaseapp.com",
+    databaseURL: "https://all-purpose-238011.firebaseio.com",
+    projectId: "all-purpose-238011",
+    storageBucket: "all-purpose-238011.appspot.com",
+    messagingSenderId: "19979990333"
 }
 
 const  firebaseApp = firebase.initializeApp(Config)
